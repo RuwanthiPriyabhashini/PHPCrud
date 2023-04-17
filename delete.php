a36b29c5d8c753740d2ecdfd3f57389bf102ce88
RuwanthiPriyabhashini <ruwanthipriyabhashini1996@gmail.com>
2023-04-18 03:25:41 +0530

    Committed

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+
+include "db_connection.php";
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM `crud` WHERE id = $id";
+
+$result= mysqli_query($conn,$sql);
+
+if($result){
+    header("Location: index.php?msg=Record deleted successfully!");
+}
+else{
+    echo "Record Deleting Failed!" . mysqli_error($conn);
+}
+
+?>
